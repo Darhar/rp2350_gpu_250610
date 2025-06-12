31e9219470d5b26eefb2b383f71494887d065508
Darhar <dlindl2003@yahoo.com>
2025-06-12 16:58:07 +0930

    clean up

--- widget.cpp
+++
@@ -1,6 +0,0 @@
-#include "widget.h"
-
-Widget::Widget(const std::string& text, int x, int y, int width, int height)
-    : boundingBox(x, y, width, height), label(text) {}
-
-Widget::~Widget() = default;
